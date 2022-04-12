5da5b545f48cf58e7c1c9d957be8c5f46d2b5de9
winwin2011 <yjh2011@live.com>
2022-04-13 01:12:36 +0800

    doc

--- src/route.ts
+++ src/route.ts
@@ -6,13 +6,35 @@ import {
 } from './middlewares'
 
 export interface AuthOption {
+  /**
+   * Verify a basic auth
+   * @param username username
+   * @param password password
+   * @returns is verified or not
+   */
   verify(username: string, password: string): Promise<boolean> | boolean
+  /**
+   * Get jwt secret
+   */
   secret(): string
 }
+
 export function createAuth({ verify, secret }: AuthOption) {
+  /**
+   * Router to provide `/login` and `/info` route
+   */
   const router = new Router()
+  /**
+   * Middleware to auth with basic auth
+   */
   const local = createLocalStrategy(verify)
+  /**
+   * Middleware to set cookie from ctx.state.user
+   */
   const cookie = createCookie(secret)
+  /**
+   * Middleware to auth
+   */
   const auth = createAuthentication(secret)
   router.prefix('/auth')
   router.post('/login', local, cookie, ctx => (ctx.status = 200))
@@ -21,5 +43,22 @@ export function createAuth({ verify, secret }: AuthOption) {
     ctx.status = 200
   })
   router.use((ctx, next) => next())
-  return { router, local, cookie, auth }
+  return {
+    /**
+     * Router to provide `/login` and `/info` route
+     */
+    router,
+    /**
+     * Middleware to auth with basic auth
+     */
+    local,
+    /**
+     * Middleware to set cookie from ctx.state.user
+     */
+    cookie,
+    /**
+     * Middleware to auth
+     */
+    auth,
+  }
 }
